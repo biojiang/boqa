9e4e07305ecd6320c8225a0d1d790db6b965dfae
Sebastian Bauer <mail@sebastianbauer.info>
2011-06-16 10:00:37 +0000

    Refactored the Util class into an own class.

--- b4o/src/sonumina/b4o/B4O.java
+++ b4o/src/sonumina/b4o/B4O.java
@@ -165,27 +165,6 @@ class Tupel
 	public Stats stat;
 }
 
-class Util
-{
-
-	/**
-	 * Returns log(a + b)
-	 * 
-	 * @param loga log(a)
-	 * @param logb log(b)
-	 * @return
-	 */
-	static double logAdd(double loga, double logb)
-	{
-		/* Addition in logspace. To see why this works consider that
-		 * log(a+b) = log(a(1+b/a)) then apply usual rules of logarithm  */
-		
-		if (Double.isInfinite(loga)) return logb;
-		return loga + Math.log(1 + Math.exp(logb - loga));
-
-	}
-}
-
 class WeightedStats implements Iterable<Tupel>
 {
 	private ArrayList<Tupel> tupelList = new ArrayList<Tupel>(10);
