6d02c361f8d61888d433763336e6d02a43748c19
Sebastian Bauer <mail@sebastianbauer.info>
2011-07-01 14:42:15 +0000

    Added test case.

--- b4oweb/test/sonumina/b4oweb/B4OCoreTest.java
+++ b4oweb/test/sonumina/b4oweb/B4OCoreTest.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
+import org.mortbay.jetty.security.B64Code;
 
 import sonumina.b4oweb.server.B4OCore;
 import sonumina.b4oweb.server.ItemResultEntry;
@@ -16,6 +17,13 @@ public class B4OCoreTest
 	@Test
 	public void test()
 	{
-		B4OCore.score(Arrays.asList(0,1,2,3));
+		int i;
+		List<ItemResultEntry> resultList = B4OCore.score(Arrays.asList(12,900));
+		for (i=0;i<resultList.size();i++)
+		{
+			int id = resultList.get(i).getItemId();
+			System.out.println(id + " " + B4OCore.getItemName(id) + " " + resultList.get(i).getScore());
+			if (i>10) break;
+		}
 	}
 }
