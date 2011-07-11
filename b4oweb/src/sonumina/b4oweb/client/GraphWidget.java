5e420e6906c5dd8bf88bdd0807b778579289e446
Sebastian Bauer <mail@sebastianbauer.info>
2011-07-11 15:23:37 +0000

    More modularization.

--- b4oweb/src/sonumina/b4oweb/client/GraphWidget.java
+++ b4oweb/src/sonumina/b4oweb/client/GraphWidget.java
@@ -1,53 +1,134 @@
 package sonumina.b4oweb.client;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.user.client.ui.Widget;
 
+class GraphJavaScriptObject extends JavaScriptObject
+{
+	protected GraphJavaScriptObject() {};
+	
+	/**
+	 * Returns the number of edges contained within the graph
+	 * represented by this object.
+	 * 
+	 * @return
+	 */
+	public final native int getNumberOfEdges() /*-{
+		return this.edges.length;
+	}-*/;
+	
+	/**
+	 * Adds a new node to the graph.
+	 * 
+	 * @param id
+	 */
+	public final native void addNode(String id) /*-{
+		this.addNode(id);
+	}-*/;
+
+	/**
+	 * Adds a new node to the graph with the given id and label l.
+	 * 
+	 * @param id the id of the node
+	 * @param l
+	 */
+	public final native void addNode(String id, String l) /*-{
+		this.addNode(id, { label : l });
+	}-*/;
+	
+	public final native void addEdge(String from, String to) /*-{
+		this.addEdge(from,to);
+	}-*/;
+	
+	/**
+	 * Creates a new graph object.
+	 */
+	public static final native GraphJavaScriptObject create() /*-{
+		var g = new $wnd.Graph();
+		return g;
+	}-*/;
+}
+
+class LayoutJavaScriptObject extends JavaScriptObject
+{
+	protected LayoutJavaScriptObject() {};
+
+	public static final native LayoutJavaScriptObject create(GraphJavaScriptObject g) /*-{
+		var layouter = new $wnd.Graph.Layout.Spring(g);
+		return layouter;
+	}-*/;
+}
+
+class RendererJavaScriptObject extends JavaScriptObject
+{
+	protected RendererJavaScriptObject() {};
+	
+	public static final native RendererJavaScriptObject create(GraphJavaScriptObject g, String id, int width, int height) /*-{
+		var renderer = new $wnd.Graph.Renderer.Raphael(id, g, width, height)
+		return renderer;
+	}-*/;
+}
+
 class GraphWidget extends Widget
 {
-	public static native void test(String id, int w, int h) /*-{
-		var width = w;
-		var height = h;
-			g = new $wnd.Graph();
-	
-		g.addNode("strawberry");
-		g.addNode("cherry");
-		g.addNode("1", { label : "Tomato" });
-		g.addNode("id35", { label : "meat\nand\ngreed" });
-		st = { directed: true, label : "Label", "label-style" : {"font-size": 20 } };
-		g.addEdge("kiwi", "penguin", st);
-		g.addEdge("strawberry", "cherry");
-		g.addEdge("cherry", "apple");
-		g.addEdge("cherry", "apple")
-		g.addEdge("1", "id35");
-		g.addEdge("penguin", "id35");
-		g.addEdge("penguin", "apple");
-		g.addEdge("kiwi", "id35");
-		g.addEdge("1", "cherry", { directed : true } );
-	  	// customize the colors of that edge
-		g.addEdge("id35", "apple", { stroke : "#bfa" , fill : "#56f", label : "Meat-to-Apple" });
-	   	// add an unknown node implicitly by adding an edge
-		g.addEdge("strawberry", "apple");
-		g.removeNode("1");
-		// layout the graph using the Spring layout implementation
-		var layouter = new $wnd.Graph.Layout.Spring(g);
-	//    // draw the graph using the RaphaelJS draw implementation
-		var renderer = new $wnd.Graph.Renderer.Raphael(id, g, width, height);
-	}-*/;
+//	public static native GraphJavaScriptObject test() /*-{
+//		var g = new $wnd.Graph();
+//	
+//		g.addNode("strawberry");
+//		g.addNode("cherry");
+//		g.addNode("1", { label : "Tomato" });
+//		g.addNode("id35", { label : "meat\nand\ngreed" });
+//		st = { directed: true, label : "Label", "label-style" : {"font-size": 20 } };
+//		g.addEdge("kiwi", "penguin", st);
+//		g.addEdge("strawberry", "cherry");
+//		g.addEdge("cherry", "apple");
+//		g.addEdge("cherry", "apple")
+//		g.addEdge("1", "id35");
+//		g.addEdge("penguin", "id35");
+//		g.addEdge("penguin", "apple");
+//		g.addEdge("kiwi", "id35");
+//		g.addEdge("1", "cherry", { directed : true } );
+//	  	// customize the colors of that edge
+//		g.addEdge("id35", "apple", { stroke : "#bfa" , fill : "#56f", label : "Meat-to-Apple" });
+//	   	// add an unknown node implicitly by adding an edge
+//		g.addEdge("strawberry", "apple");
+//		g.removeNode("1");
+//		return g;
+//	}-*/;
+//	
+//	public static native void render(GraphJavaScriptObject g, String id, int width, int height) /*-{
+//		// layout the graph using the Spring layout implementation
+//		var layouter = new $wnd.Graph.Layout.Spring(g);
+//	   	// draw the graph using the RaphaelJS draw implementation
+//		var renderer = new $wnd.Graph.Renderer.Raphael(id, g, width, height);
+//	}-*/;
+//	
+	private String myId;
+	private GraphJavaScriptObject graph;
 
 	public GraphWidget()
 	{
+		myId = "canvas";
+
 		DivElement div = Document.get().createDivElement();
-		div.setId("canvas");
+		div.setId(myId);
 		setElement(div);
+		
+		graph = GraphJavaScriptObject.create();
 	}
 
 	@Override
 	protected void onLoad() {
 		super.onLoad();
 
-		test("canvas", getElement().getClientWidth(), getElement().getClientHeight());
+		graph.addNode("Hello");
+		graph.addNode("Bello");
+		graph.addEdge("Hello", "Bello");
+		LayoutJavaScriptObject.create(graph);
+		RendererJavaScriptObject.create(graph,"canvas", getElement().getClientWidth(), getElement().getClientHeight());
 	}
 
 };
