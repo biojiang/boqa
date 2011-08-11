b2da8fccbea66d23c3b944081045ff3107ac0886
Sebastian Bauer <mail@sebastianbauer.info>
2011-08-11 09:19:06 +0000

    Added test.

---
+++ b4o/test/sonumina/b4o/calculation/tests/B4OTest.java
@@ -0,0 +1,17 @@
+package sonumina.b4o.calculation.tests;
+
+import sonumina.b4o.B4O;
+import sonumina.b4o.InternalDatafiles;
+
+public class B4OTest
+{
+	public static void testWithInternal()
+	{
+		InternalDatafiles data = new InternalDatafiles();
+	}
+
+	public static void main(String[] args)
+	{
+		testWithInternal();
+	}
+}
