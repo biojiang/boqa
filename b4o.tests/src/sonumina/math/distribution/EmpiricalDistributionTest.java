a86cefb157a0cee5279786741c2b3d0397e73e28
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-24 12:28:14 +0000

    Added new test.

--- b4o.tests/src/sonumina/math/distribution/EmpiricalDistributionTest.java
+++ b4o.tests/src/sonumina/math/distribution/EmpiricalDistributionTest.java
@@ -8,16 +8,26 @@ import sonumina.math.distribution.EmpiricalDistribution;
 
 public class EmpiricalDistributionTest
 {
-
+	
 	@Test
 	public void test()
 	{
-		double [] obs = new double[]{ 1,1,1.5,2,2.5,3,3.5,4,0.5 };
-		
+		double [] obs = new double[]{1,1,1.5,2,2.5,3,3.5,4,0.5 };
+
 		EmpiricalDistribution dis = new EmpiricalDistribution(obs);
 		 
 		assertEquals(dis.cdf(0.5,false),1/(double)obs.length,0.0001);
 		assertEquals(dis.cdf(1,false),3/(double)obs.length,0.0001);
 	}
 
+	@Test
+	public void testWithCounts()
+	{
+		double [] obs = new double[]{1, 1.5, 2, 2.5, 3, 3.5, 4, 0.5 };
+		int [] counts = new int[]{2, 1, 1, 1, 1, 1, 1, 1};
+		
+		EmpiricalDistribution dis = new EmpiricalDistribution(obs,counts);
+		assertEquals(dis.cdf(0.5,false),1/(double)obs.length,0.0001);
+		assertEquals(dis.cdf(1,false),3/(double)obs.length,0.0001);
+	}
 }
