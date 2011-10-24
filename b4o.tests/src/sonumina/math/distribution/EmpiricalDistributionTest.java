f623bb0d677298d2dc408c53e6db98c6e17656bb
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-24 13:14:26 +0000

    Added new test that tests cdf with non-observations.

--- b4o.tests/src/sonumina/math/distribution/EmpiricalDistributionTest.java
+++ b4o.tests/src/sonumina/math/distribution/EmpiricalDistributionTest.java
@@ -33,4 +33,16 @@ public class EmpiricalDistributionTest
 		assertEquals(dis.cdf(0.5,false),1/(double)totalCounts,0.0001);
 		assertEquals(dis.cdf(1,false),3/(double)totalCounts,0.0001);
 	}
+	
+	@Test
+	public void testWithNonObservations()
+	{
+		double [] obs = new double[]{1,1,1.5,2,2.5,3,3.5,4,0.5 };
+
+		EmpiricalDistribution dis = new EmpiricalDistribution(obs);
+		 
+		assertEquals(dis.cdf(0.51,false),1/(double)obs.length,0.0001);
+		assertEquals(dis.cdf(1.01,false),3/(double)obs.length,0.0001);
+	}
+
 }
