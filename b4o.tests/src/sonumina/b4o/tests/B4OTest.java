1a16c0e17b0bd67e28b499a434ae9b14aedfa253
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-24 09:06:13 +0000

    Initial check in.

---
+++ b4o.tests/src/sonumina/b4o/tests/B4OTest.java
@@ -0,0 +1,19 @@
+package sonumina.b4o.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import sonumina.b4o.InternalDatafiles;
+
+public class B4OTest {
+
+	private static InternalDatafiles data = new InternalDatafiles();
+
+	@Test
+	public void test()
+	{
+		
+	}
+}
