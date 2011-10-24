4713465643e55f80f783e6a69cb373d2e49cbb62
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-24 14:22:21 +0000

    Added not implemented approximated empirical distribution and test.

---
+++ b4o.tests/src/sonumina/math/distribution/ApproximatedEmpiricalDistributionTest.java
@@ -0,0 +1,14 @@
+package sonumina.math.distribution;
+
+import org.junit.Test;
+
+/**
+ *  A simple class representing an approximated
+ *  empirical probability.
+ * 
+ * @author Sebastian Bauer
+ */
+public class ApproximatedEmpiricalDistributionTest
+{
+	
+}
---
+++ b4o/src/sonumina/math/distribution/ApproximatedEmpiricalDistribution.java
@@ -0,0 +1,16 @@
+package sonumina.math.distribution;
+
+/**
+ *  A simple class representing an approximated
+ *  empirical probability.
+ * 
+ * @author Sebastian Bauer
+ */
+public class ApproximatedEmpiricalDistribution implements IDistribution {
+
+	@Override
+	public double cdf(double x, boolean lowerTail)
+	{
+		return 0;
+	}
+}
