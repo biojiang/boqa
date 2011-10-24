d98b8dace539efcddd6c16ccad41d6e9bfbb02c1
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-24 09:43:07 +0000

    Added new class to represent an empirical distribution.

---
+++ b4o/src/sonumina/b4o/calculation/EmpiricalDistribution.java
@@ -0,0 +1,23 @@
+package sonumina.b4o.calculation;
+
+/**
+ * A simple class representing an empirical probability
+ * distribution.
+ * 
+ * @author Sebastian Bauer
+ *
+ */
+public class EmpiricalDistribution
+{
+	private double [] observations;
+
+	/**
+	 * Constructs an empirical distribution.
+	 * 
+	 * @param observations
+	 */
+	public EmpiricalDistribution(double [] observations)
+	{
+		
+	}
+}
