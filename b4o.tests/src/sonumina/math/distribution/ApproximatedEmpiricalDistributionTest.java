5d953979321ee6ea23070ecce42bb0b51c79fb6a
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-24 15:00:30 +0000

    Added test.

--- b4o.tests/src/sonumina/math/distribution/ApproximatedEmpiricalDistributionTest.java
+++ b4o.tests/src/sonumina/math/distribution/ApproximatedEmpiricalDistributionTest.java
@@ -1,5 +1,7 @@
 package sonumina.math.distribution;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 /**
@@ -10,5 +12,19 @@ import org.junit.Test;
  */
 public class ApproximatedEmpiricalDistributionTest
 {
-	
+	@Test
+	public void test()
+	{
+		double [] obs = new double[100];
+		for (int i=0;i<obs.length;i++)
+			obs[i] = i;
+
+		ApproximatedEmpiricalDistribution dist = new ApproximatedEmpiricalDistribution(obs,10);
+		assertEquals(0.6, dist.cdf(51, false),0.001);
+		assertEquals(0.2, dist.cdf(11, false),0.001);
+		assertEquals(0.1, dist.cdf(0, false),0.001);
+		assertEquals(  0, dist.cdf(-11, false),0.001);
+		assertEquals(  1, dist.cdf(100, false),0.001);
+	}
+
 }
