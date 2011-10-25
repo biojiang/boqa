c99b90f1e35495846899a6f90f80c056c2b21688
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-25 16:23:56 +0000

    Added testChoose().

--- b4o.tests/src/sonumina/b4o/tests/B4OTest.java
+++ b4o.tests/src/sonumina/b4o/tests/B4OTest.java
@@ -14,6 +14,50 @@ public class B4OTest {
 
 	private static InternalDatafiles data = new InternalDatafiles();
 
+	/**
+	 * Tests the choose function.
+	 */
+	@Test
+	public void testChoose()
+	{
+		Random rnd = new Random();
+
+		/** Create storage where to choose numbers from */
+		int [] storage = new int[1000];
+		for (int i=0;i<storage.length;i++)
+			storage[i] = i;
+
+		int [] chosen = new int[10]; 
+		
+		for (int s=1;s<10;s++)
+		{
+			for (int t=0;t<10;t++)
+			{
+				B4O.choose(rnd,s,chosen,storage);
+
+				/* Check storage array for validity */
+				boolean [] seen = new boolean[storage.length];
+				for (int i=0;i<storage.length;i++)
+				{
+					assertEquals(false,seen[storage[i]]);
+					seen[storage[i]] = true;
+				}
+
+				
+				/* Check chosen array for validity */
+				for (int i=0;i<seen.length;i++)
+					seen[i] = false;
+
+				for (int i=0;i<s;i++)
+				{
+					assertEquals(false,seen[chosen[i]]);
+					seen[chosen[i]] = true;
+				}
+			}
+		}
+		
+	}
+
 	@Test
 	public void test()
 	{
@@ -25,7 +69,6 @@ public class B4OTest {
 		int terms = data.graph.getNumberOfTerms();
 		boolean [] obs = new boolean[terms];
 
-		
 		for (int i=0;i<B4O.items2DirectTerms[2].length;i++)
 			obs[B4O.items2DirectTerms[2][i]] = true;
 
