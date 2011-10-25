4028ac016bef708b0503a7a7a31bff49516fae88
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-25 13:32:42 +0000

    Added test.

--- b4o.tests/src/sonumina/b4o/tests/B4OTest.java
+++ b4o.tests/src/sonumina/b4o/tests/B4OTest.java
@@ -2,10 +2,13 @@ package sonumina.b4o.tests;
 
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
+import java.util.Random;
+
 import org.junit.Test;
 
 import sonumina.b4o.InternalDatafiles;
+import sonumina.b4o.calculation.B4O;
+import sonumina.b4o.calculation.B4O.Result;
 
 public class B4OTest {
 
@@ -14,6 +17,23 @@ public class B4OTest {
 	@Test
 	public void test()
 	{
+		Random rnd = new Random(2);
+
+		B4O.setConsiderFrequenciesOnly(false);
+		B4O.setup(data.graph, data.assoc);
+
+		int terms = data.graph.getNumberOfTerms();
+		boolean [] obs = new boolean[terms];
+
 		
+		for (int i=0;i<B4O.items2DirectTerms[2].length;i++)
+			obs[B4O.items2DirectTerms[2][i]] = true;
+
+		Result result = B4O.resnikScore(obs, true, rnd);
+		
+		for (int i=0;i<B4O.allItemList.size();i++)
+		{
+			System.out.println(result.getMarginal(i) + "  " + result.getScore(i));
+		}
 	}
 }
