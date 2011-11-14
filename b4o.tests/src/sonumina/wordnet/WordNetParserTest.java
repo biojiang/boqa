186641304ff29ec01dd375f5eb956797eef2069f
Sebastian Bauer <mail@sebastianbauer.info>
2011-11-14 11:45:07 +0000

    Started to implement this thing.

--- b4o.tests/src/sonumina/wordnet/WordNetParserTest.java
+++ b4o.tests/src/sonumina/wordnet/WordNetParserTest.java
@@ -2,6 +2,9 @@ package sonumina.wordnet;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class WordNetParserTest
@@ -9,6 +12,14 @@ public class WordNetParserTest
 	@Test
 	public void testWordnetParser()
 	{
-		WordNetParser.parserWordnet("../b4o/data.noun");
+		try {
+			WordNetParser.parserWordnet("../b4o/data.noun");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- b4o/src/sonumina/wordnet/WordNetParser.java
+++ b4o/src/sonumina/wordnet/WordNetParser.java
@@ -1,11 +1,69 @@
 package sonumina.wordnet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ontologizer.go.Term;
 import ontologizer.go.TermContainer;
 
 public class WordNetParser
 {
-	public static TermContainer parserWordnet(String fileName)
+	public static TermContainer parserWordnet(String fileName) throws IOException
 	{
+		BufferedReader in = new BufferedReader(new FileReader(new File(fileName)));
+		
+		String line;
+		
+		while ((line = in.readLine()) != null)
+		{
+			if (line.length() == 0) continue;
+			if (!Character.isDigit(line.charAt(0))) continue;
+
+			String [] entries = line.split(" ");
+
+			String id = entries[0];
+			
+			int wCnt = Integer.parseInt(entries[3],16);
+			ArrayList<String> words = new ArrayList<String>();
+			int wCur = 4;
+			for (int i=0;i<wCnt;i++)
+			{
+				String word = entries[wCur];
+				String lexId = entries[wCur+1];
+				words.add(word);
+				wCur+=2;
+			}
+			
+			int pCnt = Integer.parseInt(entries[wCur]);
+			int pCur = wCur + 1;
+			for (int i=0;i<pCnt;i++)
+			{
+				String pointerSymb = entries[pCur];
+
+				String synsetOffset = entries[pCur+1];
+				String pos = entries[pCur+2];
+				String st = entries[pCur+3];
+				pCur += 4;
+			}
+
+			String glos = null;
+			
+			if (entries[pCur].equals("|"))
+			{
+				int pos = line.indexOf('|');
+				if (pos > 0 && line.length() > pos + 2)
+					glos = line.substring(pos + 2);
+			}
+
+
+			System.out.println(id + "  " + words.get(0) + " " + pCnt + " " + entries[pCur] + " " + glos);
+		}
+
+		
+
 		return null;
 	}
 }
