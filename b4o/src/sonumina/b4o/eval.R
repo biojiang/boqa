d0c99829e4d78f2074767b1526e9145d378a756c
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-22 12:58:07 +0000

    Fixed typo.

--- b4o/src/sonumina/b4o/eval.R
+++ b4o/src/sonumina/b4o/eval.R
@@ -5,7 +5,7 @@ VERBOSE<-F
 
 #' Evaluates a given data frame for classification performance
 #' 
-#' @param d represents a frame, from which date is gathered
+#' @param d represents a frame, from which data is gathered
 #' @param v represents a column matrix, in which the name of the slots 
 #'        that are used for the plots of the data frame can be specified. The last
 #'        column represents whether high values are good or not.
