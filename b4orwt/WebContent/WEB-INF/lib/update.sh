26c644cb001cb074dd2488c8964ff9790980ccc6
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-27 11:09:51 +0000

    Added a script to update the lib drawer svn friendly.

---
+++ b4orwt/WebContent/WEB-INF/lib/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+#
+# Basic bash script to update the contents of this drawer
+# with a content of a plugins drawer
+#
+
+for OLDNAME in *.jar
+do
+	BASENAME=`echo $OLDNAME | sed -e s/\_.*//`
+	NEWFULLNAME=`find plugins -name "$BASENAME\_*"`
+	NEWNAME=`basename $NEWFULLNAME`
+	svn mv $OLDNAME $NEWNAME
+	cp $NEWFULLNAME NEWNAME 
+done
