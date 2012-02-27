0aa5ad9b4d79a90defa7a9583ab0be79ad798305
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-27 13:22:46 +0000

    Updated to latest RWT.

--- b4orwt/src/sonumina/b4orwt/B4ORWTConfigurator.java
+++ b4orwt/src/sonumina/b4orwt/B4ORWTConfigurator.java
@@ -2,7 +2,9 @@ package sonumina.b4orwt;
 
 import org.eclipse.rwt.application.ApplicationConfiguration;
 import org.eclipse.rwt.application.ApplicationConfigurator;
+import org.eclipse.ui.forms.internal.widgets.formtextkit.FormTextAdapterResource;
 import org.eclipse.ui.forms.internal.widgets.formtextkit.FormTextResource;
+import org.eclipse.ui.forms.internal.widgets.togglehyperlinkkit.ToggleHyperlinkAdapterResource;
 import org.eclipse.ui.forms.internal.widgets.togglehyperlinkkit.ToggleHyperlinkResource;
 import org.eclipse.ui.forms.widgets.FormText;
 import org.eclipse.ui.forms.widgets.ToggleHyperlink;
@@ -21,13 +23,15 @@ public class B4ORWTConfigurator implements ApplicationConfigurator
 		config.addThemableWidget(ToggleHyperlink.class);
 		config.addThemableWidget(FormText.class);
 		config.addResource(new ToggleHyperlinkResource());
+		config.addResource(new ToggleHyperlinkAdapterResource());
 		config.addResource(new FormTextResource());
-
+		config.addResource(new FormTextAdapterResource());
+		
 		/* Add our own styles */
 		config.addStyleSheet("org.eclipse.rap.rwt.theme.Default", "b4o.css");
 		
 		/* Add the entry point of our application */
-		config.addEntryPoint("default", B4ORWT.class);
+		config.addEntryPoint("/main", B4ORWT.class);
 //		config.addEntryPoint("default", TestRWT.class);
 	}
 }
