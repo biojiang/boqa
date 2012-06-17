f562e9648ead1e73729af18b480c36623274b81d
Sebastian Bauer <mail@sebastianbauer.info>
2012-06-17 16:31:13 +0000

    Added tests.

--- boqa.tests/src/sonumina/boqa/tests/JaccardTest.java
+++ boqa.tests/src/sonumina/boqa/tests/JaccardTest.java
@@ -2,6 +2,8 @@ package sonumina.boqa.tests;
 
 import junit.framework.Assert;
 
+import ontologizer.go.Term;
+
 import org.junit.Test;
 
 import sonumina.boqa.InternalDatafiles;
@@ -17,7 +19,14 @@ public class JaccardTest
 		boqa.setConsiderFrequenciesOnly(false);
 		boqa.setPrecalculateScoreDistribution(false);
 		boqa.setup(data.graph, data.assoc);
+
+		for (Term t : data.graph)
+			System.out.println(t.getName() + " " + data.graph.getSlimGraphView().getVertexIndex(t));
 		
-		Assert.assertEquals(1.0,boqa.jaccard(0, 0),0.0001);
+		Assert.assertEquals(1.0, boqa.jaccard(0, 0),0.0001);
+		Assert.assertEquals(1.0, boqa.jaccard(1, 1),0.0001);
+		Assert.assertEquals(0.5, boqa.jaccard(9, 12),0.0001);
+		Assert.assertEquals(0.0, boqa.jaccard(9, 10),0.0001);
+		Assert.assertEquals(0.25, boqa.jaccard(6, 14),0.0001);
 	}
 }
