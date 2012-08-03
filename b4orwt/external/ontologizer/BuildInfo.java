8407d5fcea3fb17680cdfbb9c9fb2ebe9a0f60fc
Sebastian Bauer <mail@sebastianbauer.info>
2012-08-03 09:03:06 +0000

    Added dummy BuildInfo.

---
+++ b4orwt/external/ontologizer/BuildInfo.java
@@ -0,0 +1,15 @@
+package ontologizer;
+
+public class BuildInfo {
+
+	public static String getBuildString()
+	{
+		return "Unknown";
+	}
+
+	public static String getVersion()
+	{
+		return "Unknown";
+	}
+
+}
