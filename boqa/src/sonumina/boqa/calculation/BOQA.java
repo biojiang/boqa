59c6a29f3f75f89d0de3e75910fba73bc35ce3a7
Sebastian Bauer <mail@sebastianbauer.info>
2013-11-08 19:31:31 +0000

    Moved subArray into IntArray class.

--- boqa/src/sonumina/boqa/calculation/BOQA.java
+++ boqa/src/sonumina/boqa/calculation/BOQA.java
@@ -1499,22 +1499,6 @@ public class BOQA
 	}
 
 	/**
-	 * Helper function to create sub array of length elements from
-	 * the given array.
-	 * 
-	 * @param array
-	 * @param length
-	 * @return
-	 */
-	private static int [] subArray(int [] array, int length)
-	{
-		int [] a = new int[length];
-		for (int i=0;i<length;i++)
-			a[i] = array[i];
-		return a;
-	}
-	
-	/**
 	 * A simple class maintaining ints.
 	 * 
 	 * @author Sebastian Bauer
@@ -1548,7 +1532,24 @@ public class BOQA
 		{
 			array[length++] = e;
 		}
+
+		/**
+		 * Helper function to create sub array of length elements from
+		 * the given array.
+		 * 
+		 * @param array
+		 * @param length
+		 * @return
+		 */
+		private static int [] subArray(int [] array, int length)
+		{
+			int [] a = new int[length];
+			for (int i=0;i<length;i++)
+				a[i] = array[i];
+			return a;
+		}
 		
+
 		public int [] get()
 		{
 			return subArray(array, length);
