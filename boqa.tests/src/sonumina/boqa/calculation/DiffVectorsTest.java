342ff8f10cba69e1ccba85b5690ea84ec6ed3ecd
Sebastian Bauer <mail@sebastianbauer.info>
2013-11-08 09:48:23 +0000

    Started to work on the diff vector test.

---
+++ boqa.tests/src/sonumina/boqa/calculation/DiffVectorsTest.java
@@ -0,0 +1,20 @@
+package sonumina.boqa.calculation;
+
+import org.junit.Test;
+
+public class DiffVectorsTest
+{
+	@Test
+	void testDiffVectors()
+	{
+		int numberOfTerms = 10;
+		int numberOfItems = 3;
+		int maxFrequencyTerms = 1;
+
+		int [][] items2Terms = new int[numberOfItems][];
+		double [][] items2TermFrequencies = new double[numberOfItems][];
+		int [][] item2TermFrequenciesOrder = new int[numberOfItems][];
+		int [][] items2DirectTerms = new int[numberOfItems][];
+		int [][] terms2Ancestors = new int[numberOfTerms][];
+	}
+}
