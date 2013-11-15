41d0bec3c89e66a79e9d8f7c1af1a9890c2d8ab0
Sebastian Bauer <mail@sebastianbauer.info>
2013-11-15 21:24:58 +0000

    Added setdiff test.

--- boqa/src/test/java/sonumina/boqa/calculation/UtilTest.java
+++ boqa/src/test/java/sonumina/boqa/calculation/UtilTest.java
@@ -1,5 +1,6 @@
 package sonumina.boqa.calculation;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UtilTest
@@ -7,5 +8,11 @@ public class UtilTest
 	@Test
 	public void testSetDiff()
 	{
+		int [] a = new int[]{4,3,1};
+		int [] b = new int[]{1,3};
+		
+		int [] d = Util.setDiff(a, b);
+		Assert.assertEquals(1,d.length);
+		Assert.assertEquals(4,d[0]);
 	}
 }
